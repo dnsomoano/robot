ecac7e81ccfb31d8bcc1c879faff3af92f974749
Daniel N Somoano <dnsomoano@gmail.com>
2022-11-18 16:17:05 -0500

    Initial commit.

---
+++ robot.cpp
@@ -0,0 +1,23 @@
+// robot.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	int sprockets = 0;
+    ifstream inputRobFile("example-input.txt");
+	if (inputRobFile)
+	{
+		string line;
+		while (getline(inputRobFile, line))
+		{
+			cout << line << endl;
+		}
+		inputRobFile.close();
+	}
+}
