abb4b5a5848b444eb08aa0e2133d03138cdacdb7
Daniel N Somoano <dnsomoano@gmail.com>
2022-11-19 12:26:09 -0500

    Added part class and started iterating through input file. Labeling total robots, type and starting to build tree of robot.

--- robot.cpp
+++ robot.cpp
@@ -3,23 +3,64 @@
 
 #include <iostream>
 #include <string>
+#include <string_view>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <limits>
+#include "Part.h"
 
 using namespace std;
 
+struct RobotStruct {
+	string joints = "";
+	string partNum = "";
+};
+
 int main()
 {
-	cout << "Hello, this is Cameron!" << endl;
-	int sprockets = 0;
+	vector<RobotStruct> robotsFromFile;
+	int numOfRobots = 0;
+	int totalParts = 0, totalLayers = 0;
+	Part* omnidroid = new Part();
+	Part* robotamton = new Part();
     ifstream inputRobFile("example-input.txt");
 	if (inputRobFile)
 	{
 		string line;
+		//while (getline(inputRobFile, line))
 		while (getline(inputRobFile, line))
 		{
+			if (line != "")
+			{
+				RobotStruct rb;
+				stringstream s(line);
+				getline(s, rb.joints, ' ');
+				if (s.peek() != EOF)
+				{
+					s >> rb.partNum;
+				}
+				robotsFromFile.push_back(rb);
+				//if (isdigit(static_cast<unsigned char>(line[0])))
+				//	numOfRobots = stoi(line);
+				//for (int i=0; i < 1; i++)
+				//{
+				//	if (line[0] == 'o')
+				//		omnidroid->set_name(line);
+				//	else if (line[0] == 'r')
+				//		robotamton->set_name(line);
+				//}
+			}
 			cout << line << endl;
 		}
 		inputRobFile.close();
+	}
+	numOfRobots = stoi(robotsFromFile[0].joints);
+	for (int i = 0; i < numOfRobots; i++)
+	{
+		omnidroid->set_name(robotsFromFile[1].joints);
+		totalParts = stoi(robotsFromFile[2].joints);
+		totalLayers = stoi(robotsFromFile[2].partNum);
 	}
 }
 
