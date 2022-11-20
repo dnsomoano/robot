077f1819fef4539259339f9859abb966f09fda46
Daniel N Somoano <dnsomoano@gmail.com>
2022-11-20 11:41:54 -0500

    Started working on changes for recursive solution and getting input from file.

--- Part.h
+++ Part.h
@@ -8,6 +8,7 @@ class Part
 {
 private:
 	string name;
+	int partIndex;
 	int sprockets;
 public:
 	Part* left;
@@ -15,13 +16,15 @@ public:
 	Part* parent;
 	bool isRoot;
 	Part();
-	Part(int x);
-	Part(string n, int x);
+	Part(int ptidx, int x);
+	Part(string n, int ptidx, int x);
 	virtual string get_name();
 	virtual void set_name(string n);
+	virtual int get_part_index();
+	virtual void set_part_index(int i);
 	virtual int get_sprockets();
 	virtual void set_sprockets(int i);
-	virtual void insert(string name, int sp);
+	virtual void insert(string name, int ptIdx, int sp);
 };
 
 class RobotTree
@@ -32,6 +35,6 @@ protected:
 public:
   RobotTree() { root = nullptr; size = 0; }
   int getSize() const { return size; }
-  virtual void insert(string name, int x);
+  virtual void insert(string name, int ptIdx, int x);
   //virtual void remove(int x);
 };
--- robot.cpp
+++ robot.cpp
@@ -7,15 +7,18 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <limits>
+#include <ctype.h>
 #include "Part.h"
 
 using namespace std;
 
 int main()
 {
-	vector<int> partsLookup;
+	bool isOmni = false;
+	vector<int> sprockets;
+	unordered_map<int, vector<int>> use;
 	vector<RobotTree> robots;
 	vector<pair<string, string>> robotsFromFile;
 	int numOfRobots = 0;
@@ -26,46 +29,59 @@ int main()
 	if (inputRobFile)
 	{
 		string line;
-		while (getline(inputRobFile, line))
+		//while (getline(inputRobFile, line))
+		getline(inputRobFile, line, '\n');
+		numOfRobots = stoi(line);
+		getline(inputRobFile, line, '\n');
+		getline(inputRobFile, line, '\n');
+		if (line.compare("omnidroid") == 0)
 		{
-			if (line != "")
+			isOmni = true;
+			getline(inputRobFile, line, ' ');
+			totalParts = stoi(line);
+			getline(inputRobFile, line, ' ');
+			totalLayers = stoi(line);
+			getline(inputRobFile, line, '\n');
+			cout << line << endl;
+			while (getline(inputRobFile, line, '\n'))
 			{
-				pair<string, string> p;
-				stringstream s(line);
-				getline(s, p.first, ' ');
-				if (s.peek() != EOF)
-				{
-					s >> p.second;
-				}
-				robotsFromFile.push_back(p);
+					// * Omnidroid
+				//pair<string, string> p;
+				//stringstream s(line);
+				//getline(s, p.first, ' ');
+				//if (s.peek() != EOF)
+				//{
+				//	s >> p.second;
+				//}
+				//robotsFromFile.push_back(p);
 			}
 			cout << line << endl;
 		}
 		inputRobFile.close();
 	}
-	int ifSize = robotsFromFile.size();
-	numOfRobots = stoi(robotsFromFile[0].first);
-	for (int i = 0; i < numOfRobots; i++)
-	{
-		RobotTree rbt;
-		totalParts = stoi(robotsFromFile[2].first);
-		totalLayers = stoi(robotsFromFile[2].second);
-		int l = 0;
-		for (int k = totalLayers + 3; k < ifSize && l < totalParts; k++, l++)
-		{
-			partsLookup.push_back(stoi(robotsFromFile[k].first));
-		}
-		rbt.insert(robotsFromFile[1].first, 6);
-		for (int j = 3; j < totalLayers + 3 || isalpha(robotsFromFile[j].first[0]); j++)
-		{
-			// TODO check for empty string, do not overflow to parts list.
-			rbt.insert(robotsFromFile[j].first, partsLookup[stoi(robotsFromFile[j].first)]);
-		}
-		robots.push_back(rbt);
-		cout << "Added one robot" << endl;
-		//omnidroid->set_name(robotsFromFile[1].first);
-		//omnidroid->set_sprockets(6);
-	}
+	//int ifSize = robotsFromFile.size();
+	//numOfRobots = stoi(robotsFromFile[0].first);
+	//for (int i = 0; i < numOfRobots; i++)
+	//{
+	//	RobotTree rbt;
+	//	totalParts = stoi(robotsFromFile[2].first);
+	//	totalLayers = stoi(robotsFromFile[2].second);
+	//	int l = 0;
+	//	for (int k = totalLayers + 3; k < ifSize && l < totalParts; k++, l++)
+	//	{
+	//		sprockets.push_back(stoi(robotsFromFile[k].first));
+	//	}
+	//	rbt.insert(robotsFromFile[1].first, sprockets[totalParts - 1], 6);
+	//	for (int j = 3; j < totalLayers + 3 || isalpha(robotsFromFile[j].first[0]); j++)
+	//	{
+	//		// TODO check for empty string, do not overflow to parts list.
+	//		rbt.insert("", sprockets[stoi(robotsFromFile[j].first)], stoi(robotsFromFile[j].first));
+	//	}
+	//	robots.push_back(rbt);
+	//	cout << "Added one robot" << endl;
+	//	//omnidroid->set_name(robotsFromFile[1].first);
+	//	//omnidroid->set_sprockets(6);
+	//}
 }
 
 //testing
