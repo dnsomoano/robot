6b0f98033b05c22277d281a96a75feb98dc7a988
spidermonkey752@gmail.com <spidermonkey752@gmail.com>
2022-11-21 23:52:55 -0500

    sectioned off testing data robotomaton

--- robotomatonTest.cpp
+++ robotomatonTest.cpp
@@ -3,46 +3,48 @@
 #include<iostream>
 using namespace std;
 
-
+/*
 int main(){
 
     cout<< "test\n";
     
     //  used for calculating total sprockets
-    int calcSprockets[] = {3, 2, 10, 7};
-    //  stores given values of sprockets per stage
-    //int sprockets[] = {3, 2, 10, 7};
+    //pdf test
+    //int calcSprockets[] = {3, 2, 10, 7};
+    int calcSprockets[] = {3, 11, 31, 29, 5};
     //  number of stages required
-    int stageReq[] = {0, 1, 1, 3};
+    //pdf test
+    //int stageReq[] = {0, 1, 1, 3};
+    int stageReq[] = {0, 0, 1, 2, 4};
 
     //  number of stages
-    int n = 4;
+    int n = 5;
     int i = 0;
     while (i < n){
-        /*
+        
         cout<< "test outer while\n";
         cout<< i << "iteration\n"; 
-        */
+        
         if (stageReq[i] != 0){
             int j = stageReq[i];
-            /*
+            
             //test line
             cout<< "" <<calcSprockets[i];
             cout<< "\n";
-            */
+            
             while (j > 0){
-                /*
+                
                 cout<< "test inner while\n";
                 //test line
                 cout<< "" <<calcSprockets[i];
                 cout<< "\n";
-                */
+                
                 calcSprockets[i] = calcSprockets[i] + calcSprockets[i-j];
-                /*
+                
                 //test line
                 cout<< "" <<calcSprockets[i];
                 cout<< "\n";
-                */
+                
                 j--;
             }//end innerwhile
         }//end else
@@ -50,7 +52,7 @@ int main(){
     }//end while
     //cout<< "test end\n";
     cout<< "" <<calcSprockets[n-1];
-    /*
+    
     cout<< "\n";
     cout<< "" <<calcSprockets[0];
     cout<< "\n";
@@ -60,7 +62,35 @@ int main(){
     cout<< "\n";
     cout<< "" <<calcSprockets[3];
     cout<< "\n";
-    */
+    
+    return 0;
+}//end main
+*/
+
+int main(){
+
+    cout<< "test\n";
+    
+    int calcSprockets[] = {3, 11, 31, 29, 5};
+    int stageReq[] = {0, 0, 1, 2, 4};
+
+    //  number of stages
+    int n = 5;
+    int i = 0;
+    while (i < n){
+        if (stageReq[i] != 0){
+            int j = stageReq[i];
+
+            while (j > 0){
+
+                calcSprockets[i] = calcSprockets[i] + calcSprockets[i-j];
+                j--;
+            }//end innerwhile
+        }//end else
+        i++;
+    }//end while
+    //cout<< "test end\n";
+    cout<< "" <<calcSprockets[n-1];
     return 0;
 
 
